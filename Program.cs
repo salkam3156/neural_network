3f12486e0ed026c57eca65f2756ba8a8a090a16c
salkam3156 <salkam3156@hotmail.com>
2019-12-29 21:05:41 +0100

    repo setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neural_network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
