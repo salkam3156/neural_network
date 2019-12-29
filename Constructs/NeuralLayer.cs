5ebcd3c8979da01e3d50641416d42458b4a62089
salkam3156 <salkam3156@hotmail.com>
2019-12-29 21:49:15 +0100

    defining neural layer

---
+++ Constructs/NeuralLayer.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using neural_network.Primitives;
+
+namespace neural_network.Constructs
+{
+    public class NeuralLayer
+    {
+        public List<Neuron> Neurons { get; set; } = new List<Neuron>();
+        public string Name { get; set; } = string.Empty;
+        public double Weight { get; set; }
+
+        public NeuralLayer(int neuronCount, double initialWight, string name)
+        {
+            Weight = initialWight;
+            Name = name;
+
+            Enumerable.Range(0, neuronCount).ToList().ForEach(element => Neurons.Add(new Neuron()));
+        }
+
+        public void ComputeSynapticWeights(double learningRate, double delta)
+        {
+            Neurons.ForEach(element => element.Compute(learningRate, delta));
+        }
+    }
+}
